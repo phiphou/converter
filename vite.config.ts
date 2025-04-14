3d13c99f7170d796d02f36593216ed825f99a478
phiphou <phiphou@phiphou.com>
2025-04-14 11:38:53 +0200

    update lib versions

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,11 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
-  plugins: [tailwindcss()]
+  plugins: [tailwindcss()],
+  build: {
+    minify: 'esbuild',
+    rollupOptions: {
+      treeshake: true
+    }
+  }
 })
