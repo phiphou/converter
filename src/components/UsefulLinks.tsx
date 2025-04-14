a23d25672ddf5f912410824f5c1b0a706b636d99
Nezzar KEFIF <36443340+nezz0746@users.noreply.github.com>
2025-04-14 15:15:46 +0200

    feat: add useful links

---
+++ src/components/UsefulLinks.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const CharteMunich: React.FC = () => {
+  return (
+    <div className="mxh">
+      <a target="_blank" href="https://fr.wikipedia.org/wiki/Charte_de_Munich">
+        Charte de Munich
+      </a>
+    </div>
+  );
+};
+
+export default CharteMunich;
