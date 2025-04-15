6c1ed26d1129eeb5e9fab99df3188fa411fb3078
phiphou <phiphou@phiphou.com>
2025-04-15 16:36:55 +0200

    clean dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -29,23 +29,16 @@ function App() {
       }
     }
 
-    // if set via local storage previously
     if (localStorage.getItem('color-theme')) {
       if (localStorage.getItem('color-theme') === 'light') {
-        document.documentElement.classList.add('dark')
         localStorage.setItem('color-theme', 'dark')
       } else {
-        document.documentElement.classList.remove('dark')
         localStorage.setItem('color-theme', 'light')
       }
-
-      // if NOT set via local storage previously
     } else {
       if (document.documentElement.classList.contains('dark')) {
-        document.documentElement.classList.remove('dark')
         localStorage.setItem('color-theme', 'light')
       } else {
-        document.documentElement.classList.add('dark')
         localStorage.setItem('color-theme', 'dark')
       }
     }
