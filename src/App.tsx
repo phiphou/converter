9f857bcc3c7b85ad8a1cf71efb9bf6141c08eaa1
phiphou <phiphou@phiphou.com>
2025-04-15 21:10:41 +0200

    move dark mode code to component

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,9 @@
 import './App.css'
 import Form from './components/Form'
 import mainLogo from './assets/logo.png'
-import { useEffect, useState } from 'react'
 import Dark_mode_button from './components/dark_mode_button'
 
 function App() {
-  const [dark, setDark] = useState(true)
-
-  const darkModeHandler = () => {
-    setDark(!dark)
-    if (dark) {
-      document.documentElement.setAttribute('data-theme', 'light')
-    } else {
-      document.documentElement.setAttribute('data-theme', 'dark')
-    }
-
-    const darkIcon = document.getElementById('theme-toggle-dark-icon')
-    const lightIcon = document.getElementById('theme-toggle-light-icon')
-
-    if (darkIcon && lightIcon) {
-      darkIcon.classList.toggle('hidden')
-      lightIcon.classList.toggle('hidden')
-      if (
-        localStorage.getItem('color-theme') === 'dark' ||
-        (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
-      ) {
-        darkIcon.classList.remove('hidden')
-      } else {
-        lightIcon.classList.remove('hidden')
-      }
-    }
-
-    if (localStorage.getItem('color-theme')) {
-      if (localStorage.getItem('color-theme') === 'light') {
-        localStorage.setItem('color-theme', 'dark')
-      } else {
-        localStorage.setItem('color-theme', 'light')
-      }
-    } else {
-      if (document.documentElement.classList.contains('dark')) {
-        localStorage.setItem('color-theme', 'light')
-      } else {
-        localStorage.setItem('color-theme', 'dark')
-      }
-    }
-  }
-
-  useEffect(() => {
-    if (localStorage.getItem('color-theme')) {
-      if (localStorage.getItem('color-theme') === 'light') {
-        setDark(false)
-        document.documentElement.setAttribute('data-theme', 'light')
-      } else {
-        setDark(true)
-        document.documentElement.setAttribute('data-theme', 'dark')
-      }
-    } else {
-      setDark(true)
-      document.documentElement.setAttribute('data-theme', 'dark')
-    }
-  }, [])
-
   return (
     <div className="pb-3 container">
       <div className="flex gap-2 mt-10">
@@ -72,8 +15,7 @@ function App() {
         <Form />
       </div>
       <footer className="text-center mt-1 mb-2 text-sm text-gray-500 dark:text-gray-400">
-        <Dark_mode_button dark={dark} darkModeHandler={darkModeHandler} />© {new Date().getFullYear()} @Phiphou Tous
-        droits réservés. Contribuez sur{' '}
+        <Dark_mode_button />© {new Date().getFullYear()} @Phiphou Tous droits réservés. Contribuez sur{' '}
         <a href="http://github.com/phiphou/converter" target="_blank">
           Github
         </a>
--- src/components/dark_mode_button.tsx
+++ src/components/dark_mode_button.tsx
@@ -1,7 +1,63 @@
+import { useEffect, useState } from 'react'
 import moonIcon from '../assets/icons/moon.svg'
 import sunIcon from '../assets/icons/sun.svg'
 
-function Dark_mode_button({ dark, darkModeHandler }: { dark: boolean; darkModeHandler: () => void }) {
+function Dark_mode_button() {
+  const [dark, setDark] = useState(true)
+
+  const darkModeHandler = () => {
+    setDark(!dark)
+    if (dark) {
+      document.documentElement.setAttribute('data-theme', 'light')
+    } else {
+      document.documentElement.setAttribute('data-theme', 'dark')
+    }
+
+    const darkIcon = document.getElementById('theme-toggle-dark-icon')
+    const lightIcon = document.getElementById('theme-toggle-light-icon')
+
+    if (darkIcon && lightIcon) {
+      darkIcon.classList.toggle('hidden')
+      lightIcon.classList.toggle('hidden')
+      if (
+        localStorage.getItem('color-theme') === 'dark' ||
+        (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
+      ) {
+        darkIcon.classList.remove('hidden')
+      } else {
+        lightIcon.classList.remove('hidden')
+      }
+    }
+
+    if (localStorage.getItem('color-theme')) {
+      if (localStorage.getItem('color-theme') === 'light') {
+        localStorage.setItem('color-theme', 'dark')
+      } else {
+        localStorage.setItem('color-theme', 'light')
+      }
+    } else {
+      if (document.documentElement.classList.contains('dark')) {
+        localStorage.setItem('color-theme', 'light')
+      } else {
+        localStorage.setItem('color-theme', 'dark')
+      }
+    }
+  }
+
+  useEffect(() => {
+    if (localStorage.getItem('color-theme')) {
+      if (localStorage.getItem('color-theme') === 'light') {
+        setDark(false)
+        document.documentElement.setAttribute('data-theme', 'light')
+      } else {
+        setDark(true)
+        document.documentElement.setAttribute('data-theme', 'dark')
+      }
+    } else {
+      setDark(true)
+      document.documentElement.setAttribute('data-theme', 'dark')
+    }
+  }, [])
   return (
     <button
       id="theme-toggle"
