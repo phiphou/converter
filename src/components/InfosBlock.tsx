31c079c974da9a7199b7fc498b7f3bb9f968fbb2
phiphou <phiphou@phiphou.com>
2025-04-16 09:21:18 +0200

    fix infos padding

--- src/components/InfosBlock.tsx
+++ src/components/InfosBlock.tsx
@@ -2,7 +2,7 @@ import infoIcon from '../assets/icons/info.svg'
 
 function InfosBlock({ label, info }: { label: string; info: string }) {
   return (
-    <div className="gap-1 mt-12 min-w-60 text-left flex align-middle dark:text-white text-black leading-[1.6]">
+    <div className="gap-1 mt-4 min-w-60 text-left flex align-middle dark:text-white text-black leading-[1.6]">
       <img src={infoIcon} className="w-6 h-6 mr-1 lg:mr-0" />
 
       <span dangerouslySetInnerHTML={{ __html: `${label} : ${info}` }} />
