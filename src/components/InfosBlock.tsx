a7a1c79edcb3d90fdcbcc5b0b00f40ae987a8ffb
phiphou <phiphou@phiphou.com>
2025-04-16 22:04:04 +0200

    fix info icon margin

--- src/components/InfosBlock.tsx
+++ src/components/InfosBlock.tsx
@@ -2,7 +2,7 @@ import infoIcon from "../assets/icons/info.svg"
 
 function InfosBlock({label, info}: {label: string; info: string}) {
   return (
-    <div className="mt-4 flex min-w-60 gap-1 text-left align-middle leading-[1.6] text-black dark:text-white">
+    <div className="mt-4 flex min-w-60 gap-2 text-left align-middle leading-[1.6] text-black dark:text-white">
       <img src={infoIcon} className="mr-1 h-6 w-6 lg:mr-0" />
       <span dangerouslySetInnerHTML={{__html: `${label} : ${info}`}} />
     </div>
