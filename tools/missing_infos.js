e861c3078fb55eea03f3241870d8451728089a3f
phiphou <phiphou@phiphou.com>
2025-04-17 16:46:45 +0200

    add missing infos script

---
+++ tools/missing_infos.js
@@ -0,0 +1,50 @@
+import fs from "fs"
+
+// Chemin vers ton fichier .ts
+const filePath = "../src/data/dictionaries.ts"
+
+try {
+  const fileContent = fs.readFileSync(filePath, "utf-8")
+
+  const match = fileContent.match(/{\n[.\s\w:{}"',éèà<=/\->êë'(%\\âîï)°\*\+)ç]*/gim)
+
+  if (!match) {
+    throw new Error('Impossible de trouver "dictionaries" dans le fichier.')
+  }
+
+  const dictionariesCode = match[0]
+
+  const dictionaries = eval(
+    "(" +
+      dictionariesCode
+        .replaceAll(/formater: (.*)/gm, "")
+        .replaceAll(/converter: (.*)/gm, "")
+        .replaceAll(/pluralize: (.*)/gm, "") +
+      ")"
+  )
+
+  let totalUnits = 0
+  let countWithoutInfo = 0
+  let unitsWithoutInfo = []
+
+  for (const category in dictionaries) {
+    const units = dictionaries[category]
+    for (const unitKey in units) {
+      totalUnits++
+      const unit = units[unitKey]
+      if (!unit.hasOwnProperty("not_unit") && !unit.hasOwnProperty("info")) {
+        countWithoutInfo++
+        unitsWithoutInfo.push(`${category}.${unitKey}`)
+      }
+    }
+  }
+
+  const percentageMissing = ((countWithoutInfo / totalUnits) * 100).toFixed(2)
+
+  console.log(`Units : ${totalUnits}`)
+  console.log(`Unit without "infos" field : ${countWithoutInfo}`)
+  console.log(`Percentage of missing "info" field : ${percentageMissing}%`)
+  // console.log(`\nListe des unités sans "info" :\n${unitsWithoutInfo.join("\n")}`)
+} catch (error) {
+  console.error("Erreur :", error.message)
+}
