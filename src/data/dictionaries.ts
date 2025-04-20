d4bc19968687ac3475a775ab456a4f5a6dd8cfec
phiphou <phiphou@phiphou.com>
2025-04-20 11:54:58 +0200

    add acceleration units

--- src/data/dictionaries.ts
+++ src/data/dictionaries.ts
@@ -2119,6 +2119,78 @@ export const dictionaries = {
       pluralize: false,
     },
   },
+  accélération: {
+    infos: {
+      label:
+        "accélération de surface pour les planètes selon les données de <a href='https://fr.wikipedia.org/wiki/Gravit%C3%A9_de_surface' target='_blank'>Wikipedia</a>.",
+      divisor: 999999999,
+      pluralize: false,
+    },
+    ms2: {
+      label: "m/s²",
+      divisor: 1,
+    },
+    mmin2: {
+      label: "m/min²",
+      divisor: 1 / 3600,
+    },
+    mh2: {
+      label: "m/h²",
+      divisor: 1 / 12960000,
+    },
+    kms2: {
+      label: "km/s²",
+      divisor: 1000,
+    },
+    kmmin2: {
+      label: "km/min²",
+      divisor: 1 / 3.6,
+    },
+    kmh2: {
+      label: "km/h²",
+      divisor: 1 / 12960,
+    },
+    g_earth: {
+      label: "Terre (g)",
+      divisor: 9.81,
+    },
+    g_moon: {
+      label: "Lune (g)",
+      divisor: 1.63,
+    },
+    g_sun: {
+      label: "Soleil (g)",
+      divisor: 273.95,
+    },
+    g_mercury: {
+      label: "Mercure (g)",
+      divisor: 3.701,
+    },
+    g_venus: {
+      label: "Vénus (g)",
+      divisor: 8.87,
+    },
+    g_mars: {
+      label: "Mars (g)",
+      divisor: 3.711,
+    },
+    g_jupiter: {
+      label: "Jupiter (g)",
+      divisor: 24.796,
+    },
+    g_saturn: {
+      label: "Saturne (g)",
+      divisor: 10.44,
+    },
+    g_uranus: {
+      label: "Uranus (g)",
+      divisor: 8.87,
+    },
+    g_neptun: {
+      label: "Neptune (g)",
+      divisor: 11.15,
+    },
+  },
   durée: {
     s: {
       label: "seconde",
@@ -2330,7 +2402,7 @@ export const dictionaries = {
   radioactivité: {
     infos: {
       label:
-        "Pour un calcul plus précis de votre exposition annuelle aux rayonements ionisants, utilisez le simulateur de l'<a href='https://expop.irsn.fr' target='_blank'>IRSN</a>.",
+        "pour un calcul plus précis de votre exposition annuelle aux rayonements ionisants, utilisez le simulateur de l'<a href='https://expop.irsn.fr' target='_blank'>IRSN</a>.",
       divisor: 999999999,
       pluralize: false,
     },
@@ -2371,7 +2443,7 @@ export const dictionaries = {
   radioactivité_2: {
     infos: {
       label:
-        "Pour un calcul plus précis de votre exposition annuelle aux rayonements ionisants, utilisez le simulateur de l'<a href='https://expop.irsn.fr' target='_blank'>IRSN</a>.",
+        "pour un calcul plus précis de votre exposition annuelle aux rayonements ionisants, utilisez le simulateur de l'<a href='https://expop.irsn.fr' target='_blank'>IRSN</a>.",
       divisor: 999999999,
       pluralize: false,
     },
